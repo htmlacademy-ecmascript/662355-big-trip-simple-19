b77690784591f231003b8766a21926134c91c54d
Evgeniia Dmitrieva <be230497@gmail.com>
2023-02-01 21:27:50 +0700

    Добавила блокировку фильтра

--- src/presenter/filter-presenter.js
+++ src/presenter/filter-presenter.js
@@ -1,23 +1,33 @@
 import FilterView from '../view/filter-view.js';
 import { render, replace, remove } from '../framework/render.js';
 import { FilterType, UpdateType } from '../constants.js';
+import { filter } from '../utils/filter.js';
 
 export default class FilterPresenter {
   #filterContainer = null;
   #filterModel = null;
   #filterComponent = null;
+  #pointsModel = null;
+  #filterType = FilterType.ALL;
 
-  constructor({ filterContainer, filterModel }) {
+  constructor({ filterContainer, filterModel, pointsModel }) {
     this.#filterContainer = filterContainer;
     this.#filterModel = filterModel;
-    this.#filterModel.addObserver(this.#handleModelEvent);
+    this.#filterModel.addObserver(this.#handleFilterModelEvent);
+    this.#pointsModel = pointsModel;
+    this.#pointsModel.addObserver(this.#handlePointsModelEvent);
   }
 
   init(filterType = FilterType.ALL) {
+    this.#filterType = filterType;
     const prevFilterComponent = this.#filterComponent;
+    const filterFuture = filter[FilterType.FUTURE];
+    const isDisabledFuture = filterType === FilterType.ALL &&
+      filterFuture(this.#pointsModel.points).length === 0;
     this.#filterComponent = new FilterView({
       onChange: this.#handleFilterChange,
-      filterType: filterType
+      filterType: filterType,
+      isDisabledFuture: isDisabledFuture
     });
 
     if (!prevFilterComponent) {
@@ -32,8 +42,16 @@ export default class FilterPresenter {
     this.#filterModel.setFilter(UpdateType.MAJOR, filterType);
   };
 
-  #handleModelEvent = (_updateType, filterType) => {
+  #handleFilterModelEvent = (_updateType, filterType) => {
     this.init(filterType);
   };
 
+  #handlePointsModelEvent = (updateType) => {
+    switch (updateType) {
+      case UpdateType.MAJOR:
+      case UpdateType.INIT:
+        this.init(this.#filterType);
+        break;
+    }
+  };
 }
